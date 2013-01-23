b01af97382fa400ab7c09327aa701d5586e66221
Bence Danyi <madbence@gmail.com>
2013-01-23 18:43:09 +0100

    bumm

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var irc=require('node-irc');
 var client=irc.Client('irc.atw.hu',6667);
+
 client.use('logger');
+client.use('db');
 client.use('message');
 client.use('ping');
 client.use('command');
@@ -10,10 +12,16 @@ client.use('admin',{
 client.use('uptime');
 client.use('cica');
 client.use('suna');
+client.use('rfc');
+client.use('wiki');
+client.use('toccintas');
 client.connect();
+
 setTimeout(function(){
+	client.join('#derexog');
 	client.join('#felhour');
 },2000);
+
 process.on('uncaughtException',function(e){
 	console.log('Uncaught Exception: ',e);
 	console.log(e.stack);
